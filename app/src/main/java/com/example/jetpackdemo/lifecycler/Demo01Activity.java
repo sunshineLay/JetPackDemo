ff3439dea738e796f994a905133d3dc747035f40
2491812016@qq.com <>
2021-11-18 17:31:21 +0800

    保存

--- app/src/main/java/com/example/jetpackdemo/lifecycler/Demo01Activity.java
+++ app/src/main/java/com/example/jetpackdemo/lifecycler/Demo01Activity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.jetpackdemo.R;
+
 /**
  * @TODO:
  * @Date: 2021/11/18 17:16
@@ -14,6 +16,6 @@ public class Demo01Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.demo01_life_activity);
     }
 }
