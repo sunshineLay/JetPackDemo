8e714d17b3752f49d4fd4b8bcc68305a97c89cfe
2491812016@qq.com <>
2021-11-18 17:17:18 +0800

    11-18

--- app/src/main/java/com/example/jetpackdemo/MainActivity.java
+++ app/src/main/java/com/example/jetpackdemo/MainActivity.java
@@ -8,13 +8,10 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.jetpackdemo.databinding.ActivityMainBinding;
---
+++ app/src/main/java/com/example/jetpackdemo/lifecycler/Demo01Activity.java
@@ -0,0 +1,19 @@
+package com.example.jetpackdemo.lifecycler;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @TODO:
+ * @Date: 2021/11/18 17:16
+ * @User: lay
+ */
+public class Demo01Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
