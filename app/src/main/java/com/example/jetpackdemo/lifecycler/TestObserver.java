30ba0a3801a49ce58b52cbd45ce80e2117a45fa8
2491812016@qq.com <>
2021-12-03 17:17:03 +0800

    12-3

---
+++ app/src/main/java/com/example/jetpackdemo/lifecycler/TestObserver.java
@@ -0,0 +1,22 @@
+package com.example.jetpackdemo.lifecycler;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+/**
+ * @TODO:
+ * @Date: 2021/12/3 15:29
+ * @User: lay
+ */
+public class TestObserver implements DefaultLifecycleObserver {
+    @Override
+    public void onStart(@NonNull LifecycleOwner owner) {
+
+    }
+
+    @Override
+    public void onStop(@NonNull LifecycleOwner owner) {
+
+    }
+}
